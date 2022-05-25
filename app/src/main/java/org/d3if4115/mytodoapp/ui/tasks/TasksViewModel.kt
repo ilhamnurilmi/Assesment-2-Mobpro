af637b2f3cd82cc08945c6db23d923e272e2a64d
ilhamnurilmi <ilhamnurilmi@student.telkomuniversity.ac.id>
2022-05-25 18:34:04 +0700

    Room Search

--- app/src/main/java/org/d3if4115/mytodoapp/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/org/d3if4115/mytodoapp/ui/tasks/TasksViewModel.kt
@@ -3,10 +3,19 @@ package org.d3if4115.mytodoapp.ui.tasks
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import org.d3if4115.mytodoapp.data.TaskDao
 
 class TasksViewModel @ViewModelInject constructor(
     private val taskDao: TaskDao
 ) : ViewModel() {
-    val tasks = taskDao.getTasks().asLiveData()
+
+    val searchQuery = MutableStateFlow("")
+
+    private val tasksFlow = searchQuery.flatMapLatest {
+        taskDao.getTasks(it)
+    }
+
+    val tasks = tasksFlow.asLiveData()
 }
