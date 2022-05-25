bf4a268abc828c4289ae4eacee0614b83f1a8ffa
ilhamnurilmi <ilhamnurilmi@student.telkomuniversity.ac.id>
2022-05-25 18:48:51 +0700

    Jetpack Datastore

--- app/src/main/java/org/d3if4115/mytodoapp/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/org/d3if4115/mytodoapp/ui/tasks/TasksViewModel.kt
@@ -3,18 +3,39 @@ package org.d3if4115.mytodoapp.ui.tasks
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.flatMapLatest
+import androidx.lifecycle.viewModelScope
+import org.d3if4115.mytodoapp.data.PreferencesManager
+import org.d3if4115.mytodoapp.data.SortOrder
 import org.d3if4115.mytodoapp.data.TaskDao
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.launch
 
 class TasksViewModel @ViewModelInject constructor(
-    private val taskDao: TaskDao
+    private val taskDao: TaskDao,
+    private val preferencesManager: PreferencesManager
 ) : ViewModel() {
 
     val searchQuery = MutableStateFlow("")
 
-    private val tasksFlow = searchQuery.flatMapLatest {
-        taskDao.getTasks(it)
+    val preferencesFlow = preferencesManager.preferencesFlow
+
+    private val tasksFlow = combine(
+        searchQuery,
+        preferencesFlow
+    ) { query, filterPreferences ->
+        Pair(query, filterPreferences)
+    }.flatMapLatest { (query, filterPreferences) ->
+        taskDao.getTasks(query, filterPreferences.sortOrder, filterPreferences.hideCompleted)
+    }
+
+    fun onSortOrderSelected(sortOrder: SortOrder) = viewModelScope.launch {
+        preferencesManager.updateSortOrder(sortOrder)
+    }
+
+    fun onHideCompletedClick(hideCompleted: Boolean) = viewModelScope.launch {
+        preferencesManager.updateHideCompleted(hideCompleted)
     }
 
     val tasks = tasksFlow.asLiveData()
